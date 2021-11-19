550defa524fae9ed50061632323df6925f6ff3d9
josephani <joeoani@gmail.com>
2021-11-19 15:13:39 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,67 @@
+// index.js
+import React from 'react'
+import ReactDOM from 'react-dom'
+import joseph from './images/joseph.jpg'
+
+// Header Component
+const Header = () => (
+  <header>
+    <div className='header-wrapper'>
+      <h1>Welcome to 30 Days Of React</h1>
+      <h2>Getting Started React</h2>
+      <h3>JavaScript Library</h3>
+      <p>Joseph Ani</p>
+      <small>Oct 3, 2020</small>
+    </div>
+  </header>
+)
+
+// User Card Component
+const UserCard = () => (
+  <div className='user-card'>
+    <img src={joseph} alt='joseph smiling' />
+    <h2>Joseph Ani</h2>
+  </div>
+)
+
+// TechList Component
+const TechList = () => {
+  const techs = ['HTML', 'CSS', 'JavaScript']
+  const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>)
+  return techsFormatted
+}
+
+// Main Component
+const Main = () => (
+  <main>
+    <div className='main-wrapper'>
+      <p>Prerequisite to get started react.js:</p>
+      <ul>
+        <TechList />
+      </ul>
+      <UserCard />
+    </div>
+  </main>
+)
+
+// Footer Component
+const Footer = () => (
+  <footer>
+    <div className='footer-wrapper'>
+      <p>Copyright 2020</p>
+    </div>
+  </footer>
+)
+
+// The App, or the parent or the container component
+const App = () => (
+  <div className='app'>
+    <Header />
+    <Main />
+    <Footer />
+  </div>
+)
+
+const rootElement = document.getElementById('root')
+// we render the App component using the ReactDOM package
+ReactDOM.render(<App />, rootElement)
